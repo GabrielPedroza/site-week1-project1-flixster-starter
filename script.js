00a4466710dcd3c07c567379909475c777f918ba
Gabriel Pedroza <gpaebdez@meta.com>
2023-06-16 16:57:59 -0700

    Global variables for query logic with API and overall url for fetching data

--- script.js
+++ script.js
@@ -7,3 +7,12 @@ const clearButton = document.getElementById('close-search-btn');
 const nowPlayingButton = document.getElementById('now-playing');
 const closeSearchButton = document.getElementById('close-search-btn');
 
+// Global variables
+let page = 1;
+let currentMovies = [];
+let query = '';
+
+const API_KEY = '67944a3230b59028541bfc89d5a96060';
+const BASE_URL = 'https://api.themoviedb.org/3';
+const MOVIES_ENDPOINT = '/movie/now_playing';
+
