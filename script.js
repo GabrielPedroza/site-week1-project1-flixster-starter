8142cc292b9db3afe65e1c89c3d9f4a57f20d053
Gabriel Pedroza <gpaebdez@meta.com>
2023-06-16 16:55:37 -0700

    grabbing HTML elements in JS so logic can be added

---
+++ script.js
@@ -0,0 +1,9 @@
+// DOM elements
+const moviesContainer = document.getElementById('movies-grid');
+const loadMoreButton = document.getElementById('load-more-movies-btn');
+const searchInput = document.getElementById('search-input');
+const searchButton = document.getElementById('search-button');
+const clearButton = document.getElementById('close-search-btn');
+const nowPlayingButton = document.getElementById('now-playing');
+const closeSearchButton = document.getElementById('close-search-btn');
+
