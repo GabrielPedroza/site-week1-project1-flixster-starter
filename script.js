2ac570acdbe38790af25013dd983aa42f76ec77f
Gabriel Pedroza <gpaebdez@meta.com>
2023-06-16 17:12:21 -0700

    fetchMovies function implemented | gracefully handles errors

--- script.js
+++ script.js
@@ -16,3 +16,16 @@ const API_KEY = '67944a3230b59028541bfc89d5a96060';
 const BASE_URL = 'https://api.themoviedb.org/3';
 const MOVIES_ENDPOINT = '/movie/now_playing';
 
+// Function to fetch movies from API
+const fetchMovies = async () => {
+  const url = `${BASE_URL}${MOVIES_ENDPOINT}?api_key=${API_KEY}&page=${page}`;
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+    return data.results;
+  } catch (error) {
+    console.log('Error fetching movies:', error);
+    return [];
+  }
+};
+
